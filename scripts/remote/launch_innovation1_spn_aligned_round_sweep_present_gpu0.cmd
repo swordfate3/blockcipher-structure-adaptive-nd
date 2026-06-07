2a727d4c9bbe568f5860aae2175f9b6caa6baf87
fate <2968195987@qq.com>
2026-06-07 15:23:11 +0800

    experiment: add spn aligned round sweep

---
+++ scripts/remote/launch_innovation1_spn_aligned_round_sweep_present_gpu0.cmd
@@ -0,0 +1,2 @@
+@echo off
+call C:\Users\1304Lijinlin\run_innovation1_spn_aligned_round_sweep_present_gpu0_and_push.cmd > C:\Users\1304Lijinlin\innovation1-spn-aligned-round-sweep-present-gpu0-20260607_launcher_stdout.txt 2> C:\Users\1304Lijinlin\innovation1-spn-aligned-round-sweep-present-gpu0-20260607_launcher_stderr.txt
