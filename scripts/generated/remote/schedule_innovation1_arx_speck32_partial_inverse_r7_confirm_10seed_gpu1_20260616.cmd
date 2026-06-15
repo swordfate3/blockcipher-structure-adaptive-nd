ad3cf3fcc86733f70ae40ed8b587c7dcf8e9c182
fate <2968195987@qq.com>
2026-06-16 00:31:59 +0800

    feat(innovation1): queue arx partial inverse confirm

---
+++ scripts/generated/remote/schedule_innovation1_arx_speck32_partial_inverse_r7_confirm_10seed_gpu1_20260616.cmd
@@ -0,0 +1,5 @@
+@echo off
+schtasks /Create /TN innovation1_arx_speck32_partial_inverse_r7_confirm_10seed_gpu1_20260616 /SC ONCE /ST 23:59 /TR "cmd.exe /c G:\lxy\blockcipher-structure-adaptive-nd\scripts\generated\remote\launch_innovation1-arx-speck32-partial-inverse-r7-confirm-10seed-gpu1-20260616.cmd" /F
+schtasks /Run /TN innovation1_arx_speck32_partial_inverse_r7_confirm_10seed_gpu1_20260616
+schtasks /Query /TN innovation1_arx_speck32_partial_inverse_r7_confirm_10seed_gpu1_20260616 /V /FO LIST
+schtasks /Delete /TN innovation1_arx_speck32_partial_inverse_r7_confirm_10seed_gpu1_20260616 /F
